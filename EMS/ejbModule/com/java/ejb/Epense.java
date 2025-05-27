2f593f74d0b1919dad0108bfbe9b9769f6645548
Ravikant Turi <ravikantt@infinite.com>
2025-05-27 17:17:12 +0530

    updated

---
+++ EMS/ejbModule/com/java/ejb/Epense.java
@@ -0,0 +1,20 @@
+package com.java.ejb;
+
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+
+/**
+ * Session Bean implementation class Epense
+ */
+@Stateless
+@LocalBean
+public class Epense {
+
+    /**
+     * Default constructor. 
+     */
+    public Epense() {
+        // TODO Auto-generated constructor stub
+    }
+
+}
