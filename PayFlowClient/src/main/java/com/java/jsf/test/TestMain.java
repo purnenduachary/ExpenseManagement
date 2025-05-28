d83a2006c55ddc8fac1935d6048a455a3743e11a
Ravikant Turi <ravikantt@infinite.com>
2025-05-29 01:43:30 +0530

    group module is done on client side mapping as well as add method is also implemented and testd

--- PayFlowClient/src/main/java/com/java/jsf/test/TestMain.java
+++ PayFlowClient/src/main/java/com/java/jsf/test/TestMain.java
@@ -3,6 +3,9 @@ package com.java.jsf.test;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 
+import com.java.jsf.dao.GroupDao;
+import com.java.jsf.daoImpl.GroupDaoImpl;
+import com.java.jsf.model.Group;
 import com.java.jsf.model.User;
 import com.java.jsf.util.HibernateUtil;
 
@@ -10,34 +13,50 @@ public class TestMain {
 	public static void main(String[] args) {
 		System.out.println("Test for hibernate");
 		
-		Session session = HibernateUtil.getSessionFactory().openSession();
-        Transaction tx = null;
-
-        try {
-            tx = session.beginTransaction();
-
-            // Create a new User instance
-            User user = new User();
-            user.setId(1);          // assuming ID is int or Integer
-            user.setName("John Doe");
-            user.setEmail("john.doe@example.com");
-            // set other fields if any
-
-            // Save the user object
-            session.save(user);
-
-            tx.commit();
-            System.out.println("User saved successfully!");
-        } catch (Exception e) {
-            if (tx != null) tx.rollback();
-            e.printStackTrace();
-        } finally {
-            session.close();
-        }
-
-        // Close the SessionFactory (optional, usually at app shutdown)
-        HibernateUtil.getSessionFactory().close();
+//		Session session = HibernateUtil.getSessionFactory().openSession();
+//        Transaction tx = null;
+//
+//        try {
+//            tx = session.beginTransaction();
+//
+//            // Create a new User instance
+//            User user = new User();
+//            user.setId(1);          // assuming ID is int or Integer
+//            user.setName("John Doe");
+//            user.setEmail("john.doe@example.com");
+//            // set other fields if any
+//
+//            // Save the user object
+//            session.save(user);
+//
+//            tx.commit();
+//            System.out.println("User saved successfully!");
+//        } catch (Exception e) {
+//            if (tx != null) tx.rollback();
+//            e.printStackTrace();
+//        } finally {
+//            session.close();
+//        }
+//
+//        // Close the SessionFactory (optional, usually at app shutdown)
+//        HibernateUtil.getSessionFactory().close();
+//        
+//        
+        
+		GroupTest();
+        
     }
+	
+	public static void GroupTest() {
+		
+		GroupDao gpdao=new GroupDaoImpl();
+		
+		Group group=new Group();
+		
+		group.setName("BARRA PARTY");
+		
+		gpdao.addGroup(group);
+	}
     }
 		
 	
